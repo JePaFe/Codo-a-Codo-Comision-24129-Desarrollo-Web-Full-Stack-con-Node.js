e0f94c1e3c9442e2927210f0cde332247bdf88b6
Jean Paul Ferreira <jeanpaul@jepafe.net>
2024-04-16 08:31:40 -0300

    JS 01

---
+++ js-01/js/script.js
@@ -0,0 +1,90 @@
+/* console.log("Hola JS");
+console.log(2 * 3); */
+
+// console.log("Hola JS", 2 * 3, console.log);
+
+let num;
+num = 10;
+// console.log(num);
+
+num = 15;
+// console.log(num);
+
+// mil lineas después
+
+// No usar var
+// var num = 5;
+// console.log(num);
+
+let nombre;
+let Nombre;
+
+let _nombre;
+let $nombre;
+
+let nombre2;
+
+let nombreCompleto;
+
+nombreCompleto = "Juan Perez";
+
+//---
+
+const PI = 3.1416;
+
+const IVA = 21;
+// IVA = 10.5;
+
+// console.log(IVA);
+
+// ---
+
+// let dato;
+// console.log(dato);
+
+// dato = 5.125;
+// console.log(dato, dato.toFixed(1), typeof dato);
+
+// dato = new Number(19.254);
+// console.log(dato, dato.toFixed(1), typeof dato);
+
+// dato = "Juan";
+// console.log(dato, typeof dato);
+
+// dato = true;
+// console.log(dato, typeof dato);
+
+// ---
+
+// let numero = 13.6;
+// numero = parseInt(numero);
+// // console.log(numero, typeof numero);
+
+// numero = "13.6a4";
+// console.log(numero, typeof numero);
+
+// // numero = Number(numero);
+// // console.log(numero, typeof numero);
+
+// numero = parseFloat(numero);
+// console.log(numero, typeof numero);
+
+// ---
+
+let num1 = parseInt(prompt("Ingrese un numero")); // '10'
+let num2; // undefined
+num2 = prompt("Ingrese otro numero"); // "5"
+
+console.log(typeof num1, typeof num2);
+
+num2 = parseInt(num2);
+
+let suma = num1 + num2;
+console.log(suma);
+document.write(suma);
+
+// let multi = num1 * num2;
+// console.log(multi);
+
+// let modulo = 11 % 2;
+// console.log(modulo);
